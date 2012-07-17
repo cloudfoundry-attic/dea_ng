37a41ac72446d053953753cad44c8d443d16796e
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-07-17 11:41:56 -0700

    Toggle logging verbosity for specs via environment
    Change-Id: I793f4a6769dd60921cf02c6f23fdd37748d27f96

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,16 @@ RSpec.configure do |config|
   config.include(Helpers)
 
   config.before do
-    config = Steno::Config.new \
+    steno_config = {
       :default_log_level => :all,
       :codec => Steno::Codec::Json.new,
       :context => Steno::Context::Null.new
+    }
 
-    Steno.init(config)
+    if ENV.has_key?("V")
+      steno_config[:sinks] = [Steno::Sink::IO.new(STDOUT)]
+    end
+
+    Steno.init(Steno::Config.new(steno_config))
   end
 end
