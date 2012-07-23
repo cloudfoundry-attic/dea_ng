606afc1a8e790b35a66491b8d9721c391c6dd6f1
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-07-23 16:59:47 -0700

    Add validation logic for instance
    Change-Id: I2f6e30d8795c765abd8a2b1bae7f551b2ea72fc0

--- spec/dea/instance_spec.rb
+++ spec/dea/instance_spec.rb
@@ -4,24 +4,50 @@ require "spec_helper"
 require "dea/instance"
 
 describe Dea::Instance do
+  let(:valid_attributes) do
+    {
+      "instance_index"      => 37,
+
+      "application_id"      => 37,
+      "application_version" => "some_version",
+      "application_name"    => "my_application",
+      "application_uris"    => ["foo.com", "bar.com"],
+      "application_users"   => ["john@doe.com"],
+
+      "droplet_sha1"        => "deadbeef",
+      "droplet_file"        => "file.ext",
+      "droplet_uri"         => "http://foo.com/file.ext",
+
+      "runtime_name"        => "ruby19",
+      "framework_name"      => "rails",
+
+      "limits"              => { "mem" => 1, "disk" => 2, "fds" => 3 },
+      "environment"         => { "FOO" => "BAR" },
+      "services"            => { "name" => "redis", "type" => "redis" },
+      "flapping"            => false,
+      "debug"               => "debug",
+      "console"             => "console",
+    }
+  end
+
   let(:bootstrap) do
     mock("bootstrap")
   end
 
-  let(:start_message) do
-    message = double("message")
-
-    # Fixture to make sure Dea::Instance.create_from_message doesn't throw up
-    defaults = {
-      "index"   => 0,
-      "droplet" => 1,
-    }
-
-    message.stub(:data).and_return(defaults.merge(start_message_data))
-    message
-  end
-
   describe "attributes from start message" do
+    let(:start_message) do
+      message = double("message")
+
+      # Fixture to make sure Dea::Instance.create_from_message doesn't throw up
+      defaults = {
+        "index"   => 0,
+        "droplet" => 1,
+      }
+
+      message.stub(:data).and_return(defaults.merge(start_message_data))
+      message
+    end
+
     subject(:instance) do
       Dea::Instance.new(bootstrap, Dea::Instance.translate_attributes(start_message.data))
     end
@@ -100,6 +126,53 @@ describe Dea::Instance do
       its(:flapping)    { should == false }
       its(:debug)       { should == "debug" }
       its(:console)     { should == "console" }
+    end
+  end
+
+  describe "validation" do
+    before do
+      bootstrap.stub(:runtimes).and_return(Hash.new { |*_| "runtime" })
+    end
+
+    it "should not raise when the attributes are valid" do
+      instance = Dea::Instance.new(bootstrap, valid_attributes)
+
+      expect do
+        instance.validate
+      end.to_not raise_error
+    end
+
+    it "should raise when attributes are missing" do
+      attributes = valid_attributes.dup
+      attributes.delete("application_id")
+      instance = Dea::Instance.new(bootstrap, attributes)
+
+      expect do
+        instance.validate
+      end.to raise_error
+    end
+
+    it "should raise when attributes are invalid" do
+      attributes = valid_attributes.dup
+      attributes["application_id"] = attributes["application_id"].to_s
+      instance = Dea::Instance.new(bootstrap, attributes)
+
+      expect do
+        instance.validate
+      end.to raise_error
+    end
+
+    it "should raise when the runtime is not found" do
+      attributes = valid_attributes.dup
+      attributes["runtime_name"] = "not_found"
+
+      instance = Dea::Instance.new(bootstrap, attributes)
+
+      bootstrap.should_receive(:runtimes).and_return({})
+
+      expect do
+        instance.validate
+      end.to raise_error(Dea::Instance::RuntimeNotFoundError)
     end
   end
 end
