728ea3a32df87296ad961ea298585bf11b1c28cb
mpage <mpage@rbcon.com>
2012-08-15 18:26:03 -0700

    Move crashes reaper to instance registry
    Change-Id: I072aa76926ae89ff3534941a535824c8ea0a7847

--- spec/dea/instance_registry_spec.rb
+++ spec/dea/instance_registry_spec.rb
@@ -76,4 +76,53 @@ describe Dea::InstanceRegistry do
       instance_registry.empty?.should be_false
     end
   end
+
+  describe "crash reaping" do
+    let(:crash_lifetime) { 10 }
+    let(:time_of_check) { 20 }
+
+    let(:instance_registry) do
+      Dea::InstanceRegistry.new("crash_lifetime_secs" => crash_lifetime)
+    end
+
+    before :each do
+      x = Time.now
+      x.stub(:to_i).and_return(time_of_check)
+      Time.stub(:now).and_return(x)
+    end
+
+    after :each do
+      instance_registry.reap_crashes
+    end
+
+    it "should reap crashes that are too old" do
+      [15, 5].each_with_index do |age, ii|
+        instance = register_crashed_instance(instance_registry, ii, age)
+        expect_reap_if(time_of_check - age > crash_lifetime, instance,
+                       instance_registry)
+      end
+    end
+
+    it "should reap all but the most recent crash for an app" do
+      [15, 14, 13].each_with_index do |age, ii|
+        instance = register_crashed_instance(instance_registry, 0, age)
+        expect_reap_if(ii != 0, instance, instance_registry)
+      end
+    end
+
+    def register_crashed_instance(instance_registry, app_id, state_ts)
+      instance = Dea::Instance.new(bootstrap, "application_id" => app_id)
+      instance.state = Dea::Instance::State::CRASHED
+      instance.stub(:state_timestamp).and_return(state_ts)
+      instance_registry.register(instance)
+      instance
+    end
+
+    def expect_reap_if(pred, instance, instance_registry)
+      method = pred ? :should_receive : :should_not_receive
+
+      instance.send(method, :destroy_crash_artifacts)
+      instance_registry.send(method, :unregister).with(instance)
+    end
+  end
 end
