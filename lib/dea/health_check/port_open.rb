27d199305b9e598ec3aa6b24001189c99ed01b1a
mpage <mpage@rbcon.com>
2012-08-16 14:15:28 -0700

    Add health check to test if instance port is open
    Change-Id: I6aada0a24932b2295f008950602a766aeef3b566

---
+++ lib/dea/health_check/port_open.rb
@@ -0,0 +1,71 @@
+require "eventmachine"
+
+module Dea
+  module HealthCheck
+    class PortOpen
+
+      class ConnectionNotifier < ::EM::Connection
+
+        attr_reader :deferrable
+
+        def initialize(deferrable)
+          super
+
+          @deferrable = deferrable
+        end
+
+        def connection_completed
+          deferrable.succeed
+        end
+
+        def unbind
+          # ECONNREFUSED, ECONNRESET, etc.
+          deferrable.mark_failure if error?
+        end
+      end
+
+      include EM::Deferrable
+
+      attr_reader :host
+      attr_reader :port
+
+      def initialize(host, port, retry_interval_secs = nil)
+        @host  = host
+        @port  = port
+        @timer = nil
+        @retry_interval_secs = retry_interval_secs || 0.5
+
+        setup_callbacks
+
+        yield self if block_given?
+
+        ::EM.next_tick { attempt_connect }
+      end
+
+      def mark_failure
+        @timer = ::EM::Timer.new(@retry_interval_secs) { attempt_connect }
+      end
+
+      private
+
+      def setup_callbacks
+        [:callback, :errback].each do |method|
+          send(method) { cleanup }
+        end
+      end
+
+      def attempt_connect
+        @conn = ::EM.connect(host, port, ConnectionNotifier, self)
+      end
+
+      def cleanup
+        @conn.close_connection
+
+        if @timer
+          @timer.cancel
+          @timer = nil
+        end
+      end
+    end
+  end
+end
---
+++ spec/dea/health_check/port_open_spec.rb
@@ -0,0 +1,52 @@
+# coding: UTF-8
+
+require "spec_helper"
+require "dea/health_check/port_open"
+require "vcap/common"
+
+describe Dea::HealthCheck::PortOpen do
+  let(:host) { "127.0.0.1" }
+  let(:port) { VCAP.grab_ephemeral_port }
+
+  it "should succed if someone is listening on the port" do
+    ok = run_health_check(host, port, 0.1) do
+      ::EM.start_server(host, port)
+    end
+
+    ok.should be_true
+  end
+
+  it "should fail if no-one is listening on the port" do
+    start = Time.now
+    ok = run_health_check(host, port, 0.1)
+    elapsed = Time.now - start
+
+    ok.should be_false
+    elapsed.should be_within(0.02).of(0.1)
+  end
+
+  def run_health_check(host, port, timeout, &blk)
+    success = false
+    health_check = nil
+
+    em(:timeout => 1) do
+      blk.call unless blk.nil?
+
+      health_check = Dea::HealthCheck::PortOpen.new(host, port, 0.02) do |hc|
+        hc.callback do
+          success = true
+          EM.stop
+        end
+
+        hc.errback do
+          success = false
+          EM.stop
+        end
+
+        hc.timeout(timeout)
+      end
+    end
+
+    success
+  end
+end
