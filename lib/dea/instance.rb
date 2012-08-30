331924100d7429b69cdadb4137a78c994b4a16ec
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-08-30 09:53:42 -0700

    Don't overwrite attributes when loading snapshot
    Change-Id: Ifa6f4636e4763a346b967778dcfe252338a60e2b

--- lib/dea/instance.rb
+++ lib/dea/instance.rb
@@ -129,23 +129,23 @@ module Dea
     def self.translate_attributes(attributes)
       attributes = attributes.dup
 
-      attributes["instance_index"]      = attributes.delete("index")
+      attributes["instance_index"]      ||= attributes.delete("index")
 
-      attributes["application_id"]      = attributes.delete("droplet")
-      attributes["application_version"] = attributes.delete("version")
-      attributes["application_name"]    = attributes.delete("name")
-      attributes["application_uris"]    = attributes.delete("uris")
-      attributes["application_users"]   = attributes.delete("users")
+      attributes["application_id"]      ||= attributes.delete("droplet")
+      attributes["application_version"] ||= attributes.delete("version")
+      attributes["application_name"]    ||= attributes.delete("name")
+      attributes["application_uris"]    ||= attributes.delete("uris")
+      attributes["application_users"]   ||= attributes.delete("users")
 
-      attributes["droplet_sha1"]        = attributes.delete("sha1")
-      attributes["droplet_uri"]         = attributes.delete("executableUri")
+      attributes["droplet_sha1"]        ||= attributes.delete("sha1")
+      attributes["droplet_uri"]         ||= attributes.delete("executableUri")
 
-      attributes["runtime_name"]        = attributes.delete("runtime")
-      attributes["framework_name"]      = attributes.delete("framework")
+      attributes["runtime_name"]        ||= attributes.delete("runtime")
+      attributes["framework_name"]      ||= attributes.delete("framework")
 
       # Translate environment to dictionary (it is passed as Array with VAR=VAL)
-      env = attributes.delete("env") || {}
-      attributes["environment"] = Hash[env.map do |e|
+      env = attributes.delete("env") || []
+      attributes["environment"] ||= Hash[env.map do |e|
         e.split("=", 2)
       end]
 
