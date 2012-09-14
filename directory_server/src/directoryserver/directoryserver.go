8e43489f6c36ec6003f6ea6f1fb53f098f592aba
Kowshik Prakasam <kprakasam@rbcon.com>
2012-09-14 01:29:09 -0700

    Provide an API to talk to DEA through HTTP.
    - Define API in the interface deaClient in package directoryserver.
    - Add tests for the API implementation.
    
    Change-Id: I597297b4e01d32ca258192bd3e1cafedd088fd33

--- directory_server/src/directoryserver/directoryserver.go
+++ directory_server/src/directoryserver/directoryserver.go
@@ -1,3 +1,83 @@
 package directoryserver
 
-// TODO(kowshik): Implement directory server.
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type deaClient interface {
+	Host() string
+	Port() int
+	HttpClient() *http.Client
+
+	SetHost(host string)
+	SetPort(port int)
+	SetHttpClient(httpClient *http.Client)
+
+	ConstructDeaRequest(path string, auth []string) (*http.Request, error)
+	Get(path string, auth []string) (*http.Response, error)
+}
+
+type deaClientImpl struct {
+	host       string
+	port       int
+	httpClient *http.Client
+}
+
+func (dc *deaClientImpl) Host() string {
+	return dc.host
+}
+
+func (dc *deaClientImpl) Port() int {
+	return dc.port
+}
+
+func (dc *deaClientImpl) HttpClient() *http.Client {
+	return dc.httpClient
+}
+
+func (dc *deaClientImpl) SetHost(host string) {
+	dc.host = host
+}
+
+func (dc *deaClientImpl) SetPort(port int) {
+	dc.port = port
+}
+
+func (dc *deaClientImpl) SetHttpClient(httpClient *http.Client) {
+	dc.httpClient = httpClient
+}
+
+func (dc *deaClientImpl) ConstructDeaRequest(path string,
+	auth []string) (*http.Request, error) {
+	url := fmt.Sprintf("http://%s:%d%s", dc.Host(), dc.Port(), path)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Forward the authentication credentials to the DEA.
+	req.Header["Authorization"] = auth
+	return req, nil
+}
+
+func (dc *deaClientImpl) Get(path string,
+	auth []string) (*http.Response, error) {
+	if dc.HttpClient() == nil {
+		dc.SetHttpClient(&http.Client{})
+	}
+
+	deaRequest, err := dc.ConstructDeaRequest(path, auth)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Sending HTTP request to DEA: %s", deaRequest)
+	return dc.HttpClient().Do(deaRequest)
+}
+
+func newDeaClient(host string, port int) deaClientImpl {
+	return deaClientImpl{host: host, port: port}
+}
