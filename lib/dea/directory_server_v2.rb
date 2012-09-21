f6aad82829adfdf0385c15846dff8d3832ccd175
mpage <mpage@rbcon.com>
2012-09-21 12:12:21 -0700

    Return the full uri to the go based directory server for find droplet
    Change-Id: Iab59c8178ac48d87847a479088adb4438bb96cee

---
+++ lib/dea/directory_server_v2.rb
@@ -0,0 +1,21 @@
+require "vcap/common"
+
+# Dummy class for encapsulating state related to the Go based server.
+module Dea
+  class DirectoryServerV2
+
+    attr_reader :domain
+    attr_reader :port
+    attr_reader :uuid
+
+    def initialize(domain, port)
+      @uuid   = VCAP.secure_uuid
+      @domain = domain
+      @port   = port
+    end
+
+    def external_hostname
+      "#{uuid}.#{domain}"
+    end
+  end
+end
