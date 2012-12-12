767afb8fbb180c5169a77ab9abcff4da7417fc59
Pieter Noordhuis <pcnoordhuis@gmail.com>
2012-12-11 22:41:27 -0800

    Add streaming handler based on fsnotify
    Change-Id: I1b3172d485b52595f8cbdf2478f95953f541e0b5

---
+++ go/src/directoryserver/helper_test.go
@@ -0,0 +1,8 @@
+package directoryserver
+
+import (
+	. "launchpad.net/gocheck"
+	"testing"
+)
+
+func Test(t *testing.T) { TestingT(t) }
---
+++ go/src/directoryserver/stream_handler_test.go
@@ -0,0 +1,96 @@
+package directoryserver
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	. "launchpad.net/gocheck"
+	"net"
+	"net/http"
+	"os"
+	"time"
+)
+
+type StreamHandlerSuite struct {
+	File    *os.File
+	Handler *StreamHandler
+}
+
+var _ = Suite(&StreamHandlerSuite{})
+
+func (s *StreamHandlerSuite) SetUpTest(c *C) {
+	s.CreateTempFile(c)
+}
+
+func (s *StreamHandlerSuite) TeardownTest(c *C) {
+	s.RemoveTempFile(c)
+
+	if s.Handler != nil {
+		s.Handler.File.Close()
+		s.Handler = nil
+	}
+}
+
+func (s *StreamHandlerSuite) CreateTempFile(c *C) {
+	f, err := ioutil.TempFile("", "stream-handler-suite")
+	c.Assert(err, IsNil)
+
+	s.File = f
+}
+
+func (s *StreamHandlerSuite) RemoveTempFile(c *C) {
+	s.File.Close()
+	os.Remove(s.File.Name())
+}
+
+func (s *StreamHandlerSuite) Get(c *C) *http.Response {
+	f, err := os.Open(s.File.Name())
+	c.Assert(err, IsNil)
+
+	_, err = f.Seek(0, os.SEEK_END)
+	c.Assert(err, IsNil)
+
+	s.Handler = &StreamHandler{
+		File:          f,
+		FlushInterval: 1 * time.Millisecond,
+	}
+
+	l, err := net.Listen("tcp", "localhost:")
+	c.Assert(err, IsNil)
+
+	x := http.Server{Handler: s.Handler}
+	go x.Serve(l)
+	defer l.Close()
+
+	res, err := http.Get(fmt.Sprintf("http://%s/", l.Addr()))
+	c.Assert(err, IsNil)
+
+	return res
+}
+
+func (s *StreamHandlerSuite) TestStream(c *C) {
+	res := s.Get(c)
+	c.Check(res.StatusCode, Equals, 200)
+
+	// The header was already sent, now write something to the file
+	fmt.Fprintf(s.File, "hello\n")
+
+	r := bufio.NewReader(res.Body)
+	l, err := r.ReadString('\n')
+	c.Check(err, IsNil)
+	c.Check(l, Equals, "hello\n")
+}
+
+func (s *StreamHandlerSuite) TestStreamFromCurrentPosition(c *C) {
+	fmt.Fprintf(s.File, "hello\n")
+
+	res := s.Get(c)
+	c.Check(res.StatusCode, Equals, 200)
+
+	fmt.Fprintf(s.File, "world\n")
+
+	r := bufio.NewReader(res.Body)
+	l, err := r.ReadString('\n')
+	c.Check(err, IsNil)
+	c.Check(l, Equals, "world\n")
+}
