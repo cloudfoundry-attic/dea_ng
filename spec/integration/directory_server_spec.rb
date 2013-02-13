d3365812c8ba958460330387dc0d53aad19de19b
Dmitriy Kalinin and Kowshik Prakasam <pair+dmitriy+kowshik@pivotallabs.com>
2013-02-13 09:58:01 -0800

    first step in adding more small integration tests for dea
    Change-Id: Ibe090fef97104e99d3f0995613ad1d00f0dc1cf5

---
+++ spec/integration/directory_server_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+require "net/http"
+
+describe "Directory server", :type => :integration do
+  start_nats
+  start_dea
+  start_directory_server
+
+  it "asks dea to verify instance file paths" do
+    result = Net::HTTP.get(URI.parse("http://localhost:5678/instance_paths/instance-id?path=/file"))
+    result.should include("Invalid HMAC")
+  end
+
+  it "asks dea to verify staging tasks file paths" do
+    result = Net::HTTP.get(URI.parse("http://localhost:5678/staging_tasks/task-id/file_path?path=/file"))
+    result.should include("Invalid HMAC")
+  end
+end
