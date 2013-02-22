01ae54302775f5af0c7f6654ded46422d64e8827
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2013-02-22 14:25:19 +0200

    Small fix for CPU stats collection.

--- lib/dea/instance.rb
+++ lib/dea/instance.rb
@@ -746,7 +746,7 @@ module Dea
           :ns_used      => info_resp.cpu_stat.usage,
         }
 
-        @cpu_samples.unshift if @cpu_samples.size > 2
+        @cpu_samples.shift if @cpu_samples.size > 2
 
         if @cpu_samples.size == 2
           used = @cpu_samples[1][:ns_used] - @cpu_samples[0][:ns_used]
