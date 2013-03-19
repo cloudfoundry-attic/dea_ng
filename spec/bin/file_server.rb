6a8f8d2ed3b86073343de8c16e874c12f3c9b253
Ryan Spore and Scott Calvert <pair+ryan+scott@pivotallabs.com>
2013-03-19 09:51:14 -0700

    Allow FileServer to serve buildpacks in git format

--- spec/bin/file_server.rb
+++ spec/bin/file_server.rb
@@ -3,9 +3,10 @@
 require "thin"
 require "sinatra/base"
 
-class FileServer < Sinatra::Base
-  APPS_DIR = File.expand_path("../../fixtures/apps", __FILE__)
+APPS_DIR = File.expand_path("../../fixtures/apps", __FILE__)
+BUILDPACKS_DIR = File.expand_path("../../fixtures/fake_buildpacks", __FILE__)
 
+class FileServer < Sinatra::Base
   get "/unstaged/:name" do |name|
     zip_path = "/tmp/fixture-#{name}.zip"
     Dir.chdir("#{APPS_DIR}/#{name}") do
@@ -18,4 +19,13 @@ class FileServer < Sinatra::Base
   end
 end
 
-FileServer.run!(:port => 9999)
+app = Rack::Builder.new do
+  map "/buildpacks" do
+    run Rack::Directory.new(BUILDPACKS_DIR)
+  end
+
+  run FileServer.new
+end
+
+$stdout.sync = true
+Rack::Handler::Thin.run(app, :Port => 9999)
