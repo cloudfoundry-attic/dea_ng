50a2c2c539a9264470e87c245643f96d963ae205
David Sabeti and Max Brunsfeld <pair+david+max@pivotallabs.com>
2013-03-21 15:20:43 -0700

    In staging integration spec, check that app was uploaded

--- spec/integration/staging_spec.rb
+++ spec/integration/staging_spec.rb
@@ -7,7 +7,7 @@ describe "Staging an app", :type => :integration, :requires_warden => true do
     let(:unstaged_url) { "http://localhost:9999/unstaged/sinatra" }
     let(:staged_url) { "http://localhost:9999/staged/sinatra" }
 
-    it "works" do
+    it "packages a ruby binary and the app's gems" do
       response = nats.request("staging", {
         "async" => false,
         "app_id" => "some-app-id",
@@ -18,6 +18,27 @@ describe "Staging an app", :type => :integration, :requires_warden => true do
 
       response["task_log"].should include("Your bundle is complete!")
       response["error"].should be_nil
+
+      download_tgz(staged_url) do |dir|
+        Dir.entries("#{dir}/app/vendor").should include("ruby-1.9.2")
+        Dir.entries("#{dir}/app/vendor/bundle/ruby/1.9.1/gems").should =~ %w(
+          .
+          ..
+          bundler-1.3.2
+          rack-1.5.1
+          rack-protection-1.3.2
+          sinatra-1.3.4
+          tilt-1.3.3
+        )
+      end
+    end
+
+    def download_tgz(url)
+      dir = Dir.mktmpdir
+      system "curl #{url} > #{dir}/staged_app.tgz"
+      system "cd #{dir} && tar xzvf staged_app.tgz 1> /dev/null"
+      yield dir
+      FileUtils.rm_rf(dir)
     end
   end
 end
