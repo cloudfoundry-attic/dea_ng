47e666a0c898ea38cb3127ad79889f056845b91a
Max Brunsfeld and Ryan Spore <pair+max+ryan@pivotallabs.com>
2013-03-22 11:48:23 -0700

    Add integration test for custom buildpack urls

--- spec/integration/staging_spec.rb
+++ spec/integration/staging_spec.rb
@@ -1,27 +1,29 @@
 require "spec_helper"
 
 describe "Staging an app", :type => :integration, :requires_warden => true do
+  let(:dea_hostname) { `hostname -I`.split(" ")[0] }
   let(:nats) { NatsHelper.new }
 
   describe "staging a simple sinatra app" do
-    let(:unstaged_url) { "http://localhost:9999/unstaged/sinatra" }
-    let(:staged_url) { "http://localhost:9999/staged/sinatra" }
+    let(:unstaged_url) { "http://#{dea_hostname}:9999/unstaged/sinatra" }
+    let(:staged_url) { "http://#{dea_hostname}:9999/staged/sinatra" }
 
-    it "packages a ruby binary and the app's gems" do
-      response = nats.request("staging", {
-        "async" => false,
-        "app_id" => "some-app-id",
-        "properties" => {},
-        "download_uri" => unstaged_url,
-        "upload_uri" => staged_url
-      })
+    context 'when the DEA has to detect the buildback' do
+      it "packages a ruby binary and the app's gems" do
+        response = nats.request("staging", {
+          "async" => false,
+          "app_id" => "some-app-id",
+          "properties" => {},
+          "download_uri" => unstaged_url,
+          "upload_uri" => staged_url
+        })
 
-      response["task_log"].should include("Your bundle is complete!")
-      response["error"].should be_nil
+        response["task_log"].should include("Your bundle is complete!")
+        response["error"].should be_nil
 
-      download_tgz(staged_url) do |dir|
-        Dir.entries("#{dir}/app/vendor").should include("ruby-1.9.2")
-        Dir.entries("#{dir}/app/vendor/bundle/ruby/1.9.1/gems").should =~ %w(
+        download_tgz(staged_url) do |dir|
+          Dir.entries("#{dir}/app/vendor").should include("ruby-1.9.2")
+          Dir.entries("#{dir}/app/vendor/bundle/ruby/1.9.1/gems").should =~ %w(
           .
           ..
           bundler-1.3.2
@@ -30,6 +32,38 @@ describe "Staging an app", :type => :integration, :requires_warden => true do
           sinatra-1.3.4
           tilt-1.3.3
         )
+        end
+      end
+    end
+
+    context "when a buildpack url is specified" do
+      let(:buildpack_url) { "http://#{dea_hostname}:9999/buildpacks/with_start_cmd/succeed_to_detect/.git" }
+
+      it "downloads the buildpack and runs it" do
+        setup_fake_buildpack
+
+        response = nats.request("staging", {
+          "async" => false,
+          "app_id" => "some-app-id",
+          "properties" => {
+            "buildpack" => buildpack_url
+          },
+          "download_uri" => unstaged_url,
+          "upload_uri" => staged_url
+        })
+
+        response["task_log"].should include("Some compilation output")
+        response["error"].should be_nil
+      end
+
+      def setup_fake_buildpack
+        Dir.chdir("spec/fixtures/fake_buildpacks/with_start_cmd/succeed_to_detect") do
+          `rm -rf .git`
+          `git init`
+          `git add . && git add -A`
+          `git commit -am "fake commit"`
+          `git update-server-info`
+        end
       end
     end
 
