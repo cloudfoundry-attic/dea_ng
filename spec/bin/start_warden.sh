b697b9ce9e1dd2f082de2fbdcf5c42bd409a6170
David Sabeti and Dmitriy Kalinin <pair+david+dmitriy@pivotallabs.com>
2013-03-29 12:39:59 -0700

    use sleep to fake start_warden in travis

--- spec/bin/start_warden.sh
+++ spec/bin/start_warden.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [ $TRAVIS_BRANCH ]
 then
     # Currently warden does not work on Travis
     # so let's just sit here forever until foreman is killed.
     # (Tests that require Warden are skipped.)
-    read
+    while true; do sleep 100000; done
 else
     # as configured in the Vagrant VM
     WARDEN_DIR=/warden/warden
