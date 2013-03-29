b6d6b33f61bb306e57a0d92893c084904cab52d3
Dmitriy Kalinin <pair+dmitriy@pivotallabs.com>
2013-03-29 09:49:09 -0700

    do not run warden on travis

--- spec/bin/start_warden.sh
+++ spec/bin/start_warden.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 set -e
-set -x
 
-# as configured in the Vagrant VM
-WARDEN_DIR=/warden/warden
+if [ $TRAVIS_BRANCH ]
+then
+    # Currently warden does not work on Travis
+    # so let's just sit here forever until foreman is killed.
+    # (Tests that require Warden are skipped.)
+    read
+else
+    # as configured in the Vagrant VM
+    WARDEN_DIR=/warden/warden
 
-cd $WARDEN_DIR
-bundle exec rake warden:start[config/dea_vm.yml]
+    cd $WARDEN_DIR
+    bundle exec rake warden:start[config/dea_vm.yml]
+fi
