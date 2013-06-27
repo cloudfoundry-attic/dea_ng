f6617c539f5b6f423b8c9bd9dacb407142ac6c71
Alex Suraci and Jon Stevens <pair+alex+jstevens@pivotallabs.com>
2013-06-26 17:41:18 -0700

    stub greet_router

--- spec/support/bootstrap_setup.rb
+++ spec/support/bootstrap_setup.rb
@@ -39,6 +39,7 @@ shared_context "bootstrap_setup" do
 
     bootstrap.stub(:start_component)
     bootstrap.stub(:start_directory_server)
+    bootstrap.stub(:greet_router)
     bootstrap.stub(:register_directory_server_v2)
     bootstrap.stub(:start_finish)
 
