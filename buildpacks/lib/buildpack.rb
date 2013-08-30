0ca6b0a8c3e8c06df66ee3eb1c64a456f0c8633e
fraenkel <fraenkel@us.ibm.com>
2013-08-30 16:35:51 -0400

    Clone entire git repo if shallow clone fails

--- buildpacks/lib/buildpack.rb
+++ buildpacks/lib/buildpack.rb
@@ -117,13 +117,18 @@ module Buildpacks
 
     def clone_buildpack(buildpack_url)
       buildpack_uri = URI.parse(buildpack_url)
-      fragment = URI.parse(buildpack_url).fragment
+      git_branch_tag = URI.parse(buildpack_url).fragment
       buildpack_uri.fragment = nil
       buildpack_url = buildpack_uri.to_s
       buildpack_path = "/tmp/buildpacks/#{File.basename(buildpack_url)}"
-      git_branch_tag = fragment ? "-b #{fragment}" : ""
-      ok = system("git clone --depth 1 #{git_branch_tag} --recursive #{buildpack_url} #{buildpack_path}")
-      raise "Failed to git clone buildpack" unless ok
+      git_branch_option = git_branch_tag ? "-b #{git_branch_tag}" : ""
+      ok = system("git clone --depth 1 #{git_branch_option} --recursive #{buildpack_url} #{buildpack_path}")
+      if !ok
+        ok = system("git clone --recursive #{buildpack_url} #{buildpack_path}")
+        raise "Failed to git clone buildpack" unless ok
+        ok = system("git checkout #{git_branch_tag}")
+        raise "Failed to git checkout buildpack" unless ok
+      end
       Buildpacks::Installer.new(Pathname.new(buildpack_path), app_dir, cache_dir)
     end
 
--- spec/unit/buildpack/buildpack_spec.rb
+++ spec/unit/buildpack/buildpack_spec.rb
@@ -262,7 +262,7 @@ fi
       subject { build_pack.build_pack }
 
       it "clones the buildpack URL" do
-        build_pack.should_receive(:system).with(anything) do |cmd|
+        build_pack.should_receive(:system) do |cmd|
           expect(cmd).to match /git clone --depth 1  --recursive #{buildpack_url} \/tmp\/buildpacks/
           true
         end
@@ -274,11 +274,31 @@ fi
         buildpack_url = "git://github.com/heroku/heroku-buildpack-java.git#branch"
         config["environment"]["buildpack"] = buildpack_url
         git_url, branch = buildpack_url.split('#')
-        build_pack.should_receive(:system).with(anything) do |cmd|
+        build_pack.should_receive(:system) do |cmd|
           expect(cmd).to match /git clone --depth 1 -b #{branch} --recursive #{git_url} \/tmp\/buildpacks/
           true
         end
 
+        subject
+      end
+
+      it "clones the buildpack URL and does a checkout with a branch" do
+        buildpack_url = "git://github.com/heroku/heroku-buildpack-java.git#49f320c5f8178279dd58af6de5ad525b72cc79d2"
+        config["environment"]["buildpack"] = buildpack_url
+        git_url, branch = buildpack_url.split('#')
+        build_pack.should_receive(:system) do |cmd|
+          expect(cmd).to eq("git clone --depth 1 -b #{branch} --recursive #{git_url} /tmp/buildpacks/heroku-buildpack-java.git")
+          false
+        end
+        build_pack.should_receive(:system) do |cmd|
+          expect(cmd).to eq("git clone --recursive #{git_url} /tmp/buildpacks/heroku-buildpack-java.git")
+          true
+        end
+        build_pack.should_receive(:system) do |cmd|
+          expect(cmd).to eq("git checkout #{branch}")
+          true
+        end
+        
         subject
       end
 
