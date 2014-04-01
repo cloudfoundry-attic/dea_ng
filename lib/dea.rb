6564c2be5cb98e260cd67b38e009ea347a1cc951
Eric Malm <emalm@pivotallabs.com>
2014-03-31 17:15:33 -0700

    Remove unused code
    Signed-off-by: Aram Price <aprice@pivotallabs.com>

--- lib/dea.rb
+++ lib/dea.rb
@@ -1,17 +1,2 @@
 module Dea
-  def self.with_em(&blk)
-    if EM.reactor_running?
-      blk.call
-    else
-      EM.run do
-        Fiber.new do
-          begin
-            blk.call
-          ensure
-            EM.stop
-          end
-        end.resume
-      end
-    end
-  end
 end
