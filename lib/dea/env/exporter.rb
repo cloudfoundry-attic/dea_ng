cc2d7cbe16a9dcb93c1f346052c09d90e6245437
Mike Heath <heathma@ldschurch.org>
2014-09-23 16:42:43 -0600

    Escape env values rather than simply quote them.

--- lib/dea/env/exporter.rb
+++ lib/dea/env/exporter.rb
@@ -1,9 +1,11 @@
+require 'shellwords'
+
 module Dea
   class Env
     class Exporter < Struct.new(:variables)
       def export
         variables.map do |(key, value)|
-          %Q{export %s="%s";\n} % [key, value.to_s.gsub('"', '\"')]
+          %Q{export %s=%s;\n} % [key, Shellwords.shellescape(value.to_s)]
         end.join
       end
     end
