b6c070b7b2a6c6322c31fb76c4ca5270a4be2720
Natalie J. Bennett <nbennett@pivotal.io>
2015-09-08 11:48:13 -0700

    Revert "Revert "Revert "Revert "Revert "Bump warden client to 0.1.2"""""
    This reverts commits 5cd3d07..db9521a
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- spec/unit/task_spec.rb
+++ spec/unit/task_spec.rb
@@ -138,14 +138,6 @@ describe Dea::Task do
     end
   end
 
-  describe "#destroy" do
-    it "closes all connections" do
-      expect(task.container).to receive(:close_all_connections)
-
-      task.destroy
-    end
-  end
-
   describe "#consuming_memory?" do
     it "returns true" do
       expect(task.consuming_memory?).to be_true
