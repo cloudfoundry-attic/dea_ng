472a21aeec4898c8639defd53a9272f730de4460
Jonathan Berkhahn <jaberkha@us.ibm.com>
2016-05-11 10:27:28 -0700

    Limit retries in callback during staging response
    - The callback is used for test purposes only. There were many attempts
      to have eventmachine play nicely with us but nothing would properly
      return the responses that we wanted.
    
    [Finishes #118500417]
    
    Signed-off-by: Dan Lavine <dlavine@us.ibm.com>

--- lib/dea/utils/cloud_controller_client.rb
+++ lib/dea/utils/cloud_controller_client.rb
@@ -3,6 +3,7 @@ require 'dea/utils/uri_cleaner'
 module Dea
   class CloudControllerClient
     INACTIVITY_TIMEOUT = 300.freeze
+    MAX_RETRIES = 3.freeze
 
     attr_reader :logger
 
@@ -12,30 +13,44 @@ module Dea
       @logger = custom_logger || self.class.logger
     end
 
-    def send_staging_response(response)
-      destination = "#{@destination}/internal/dea/staging/#{response[:app_id]}/completed"
-      logger.info('cloud_controller.staging_response.sending', destination: URICleaner.clean(destination))
+    def send_staging_response(response, &blk)
+      @cb = blk
 
       response = response.merge( {:dea_id => @uuid} )
+      send(response, 1)
+    end
 
-      http = EM::HttpRequest.new(destination, inactivity_timeout: INACTIVITY_TIMEOUT).post( head: { 'content-type' => 'application/json' }, body: Yajl::Encoder.encode(response))
+    private
 
-      http.errback do
-        handle_error(http)
-      end
+    def send(response, iteration)
+      if iteration > MAX_RETRIES
+        @cb.call(iteration) if @cb
+      else
+        destination = URI.join(@destination, "/internal/dea/staging/#{response[:app_id]}/completed")
+        logger.info('cloud_controller.staging_response.sending', destination: URICleaner.clean(destination), iteration: iteration)
 
-      http.callback do
-        handle_http_response(http)
+        http = EM::HttpRequest.new(destination, inactivity_timeout: INACTIVITY_TIMEOUT).post( head: { 'content-type' => 'application/json' }, body: Yajl::Encoder.encode(response))
+
+        http.errback do
+          if handle_error(http)
+            send(response, iteration + 1)
+          end
+        end
+
+        http.callback do
+          if handle_http_response(http)
+            send(response, iteration + 1)
+          end
+        end
       end
     end
-    
-    private 
 
     def handle_http_response(http)
       http_status = http.response_header.status
 
       if http_status == 200
         logger.info('cloud_controller.staging_response.accepted', http_status: http_status)
+        return false
       else
         handle_error(http)
       end
@@ -50,6 +65,12 @@ module Dea
                   http_error: http.error,
                   http_status: http.response_header.status,
                   http_response: http.response)
+
+      if http.response_header.status == 404 || http.response_header.status >= 500
+        return true
+      end
+
+      return false
     end
   end
 end
--- spec/unit/utils/cloud_controller_client_spec.rb
+++ spec/unit/utils/cloud_controller_client_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 require 'dea/utils/cloud_controller_client'
-require 'dea/protocol'
+require 'thin'
 
 module Dea
   describe CloudControllerClient do
     around do |example|
-      with_event_machine { example.call }
+      with_event_machine(timeout: 20) { example.call }
     end
 
-
     let(:uuid) { 'dea_id' }
-    let(:polling_timeout_in_second) { 3 }
-    let(:logger) { double(:logger, info: nil, warn: nil, debug: nil) }
+    let(:cc_uri) { "http://127.0.0.1:#{port}" }
+    let(:to_uri) { "#{cc_uri}/internal/dea/staging/#{response[:app_id]}/completed" }
+    subject { CloudControllerClient.new(uuid, cc_uri, nil) }
 
     let(:response) {
       {
@@ -24,35 +24,69 @@ module Dea
         app_id: 'asdf-qwer-zxcv',
       }
     }
-
     let(:expected_response) { response.merge( {:dea_id => 'dea_id'} ) }
 
-    let(:cc_uri) { 'http://127.0.0.1:12345' }
-    let(:to_uri) { "#{cc_uri}/internal/dea/staging/#{response[:app_id]}/completed" }
-
-    subject { CloudControllerClient.new(uuid, cc_uri, logger) }
+    let(:port) { 25432 }
+    def http_server(response_code)
+      Thin::Server.new('0.0.0.0', 25432, lambda do |env|
+        [response_code,          # Status code
+         {             # Response headers
+          'Content-Type' => 'text/html',
+          'Content-Length' => '2',
+        },
+        ['hi']]
+      end)
+    end
 
     describe "#send_staging_response" do
-      let(:request) { double(:request, method: 'post').as_null_object }
-      let(:http) { double(:http, req: request).as_null_object }
-
-      let(:status) { 200 }
-
       context "when everything works perfectly" do
+        let(:request) { double(:request, method: 'post').as_null_object }
         let(:http) { double(:http, req: request, response_header: { status: status }).as_null_object }
+        let(:status) { 200 }
+
         it "logs the success" do
-          expect(EM::HttpRequest).to receive(:new).with(to_uri, inactivity_timeout: 300).and_return(http)
+          expect(EM::HttpRequest).to receive(:new).with(URI.parse(to_uri), inactivity_timeout: 300).and_return(http)
           expect(http).to receive(:post).with(
-                            head: { 'content-type' => 'application/json' },
-                            body: Yajl::Encoder.encode(expected_response)
-                          )
-          expect(subject.logger).to receive(:info).with('cloud_controller.staging_response.sending', hash_including(:destination))
+            head: { 'content-type' => 'application/json' },
+            body: Yajl::Encoder.encode(expected_response)
+          )
+          expect(subject.logger).to receive(:info)
           expect(http).to receive(:callback)
           expect(http).to receive(:errback)
 
           subject.send_staging_response(response)
 
           done
+        end
+      end
+
+      context 'when we receive an error code 500 back' do
+        before do
+          http_server(500).start
+        end
+
+        it 'retries up to 3 times' do
+          subject.send_staging_response(response) do |iteration|
+            # 4 because we star at index 1 and we want to run with 1, 2. 3
+            expect(iteration).to eq(4)
+            done
+          end
+        end
+      end
+
+      context 'when the server is unreachable' do
+        before do
+          # if the server is not running eventmachine gives us a status code of
+          # 0, but only in test.
+          http_server(404).start
+        end
+
+        it 'retries up to 3 times' do
+          subject.send_staging_response(response) do |iteration|
+            # 4 because we star at index 1 and we want to run with 1, 2. 3
+            expect(iteration).to eq(4)
+            done
+          end
         end
       end
     end
